c435ccdfe740e4b3197772212cf406ce000f30fe
Jeremy Ng <suburbanninja575@gmail.com>
2023-08-03 14:42:39 -0400

    Update __init__.py

--- ggt/train/__init__.py
+++ ggt/train/__init__.py
@@ -1,3 +1,4 @@
 from .create_trainer import create_trainer
+from .create_optuna_trainer import create_optuna_trainer
 
-__all__ = ["create_trainer"]
+__all__ = ["create_trainer", "create_optuna_trainer"]
