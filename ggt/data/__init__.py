1bbd6ce86864cedb4789f00f0b9143771205afe5
Jeremy Ng <suburbanninja575@gmail.com>
2023-08-03 14:52:39 -0400

    Update __init__.py

--- ggt/data/__init__.py
+++ ggt/data/__init__.py
@@ -1,12 +1,14 @@
 from .dataset import FITSDataset
+from .bin_dataset import BinaryFITSDataset
 
 from torch.utils.data import DataLoader
 
-
 def get_data_loader(dataset, batch_size, n_workers, shuffle=True):
-    return DataLoader(
-        dataset, batch_size, shuffle=shuffle, num_workers=n_workers
+    return DataLoader(dataset,
+                      batch_size=int(batch_size),
+                      shuffle=shuffle,
+                      num_workers=n_workers,
     )
 
 
-__all__ = ["FITSDataset", "get_data_loader"]
+__all__ = ["FITSDataset", "BinaryFITSDataset", "get_data_loader"]
